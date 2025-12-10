efc4e58a082a078470971d0bc4fa0517dc32de41
petrkopylov <petr.a.kopylov@gmail.com>
2025-12-10 17:35:38 +0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,31 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+    let filePath = '.' + req.url;
+    if (filePath === './') filePath = './index.html';
+    
+    const extname = path.extname(filePath);
+    let contentType = 'text/html';
+    
+    switch (extname) {
+        case '.js': contentType = 'text/javascript'; break;
+        case '.css': contentType = 'text/css'; break;
+        case '.glb': contentType = 'model/gltf-binary'; break;
+    }
+    
+    fs.readFile(filePath, (error, content) => {
+        if (error) {
+            res.writeHead(404);
+            res.end('File not found');
+        } else {
+            res.writeHead(200, { 'Content-Type': contentType });
+            res.end(content);
+        }
+    });
+});
+
+server.listen(3000, () => {
+    console.log('Server running at http://localhost:3000/');
+});
